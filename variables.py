ac017b34aa705066fd5cbc22e9927e3ddc380b47
delvamar001 <delvamar001@warren.k12.in.us>
2024-08-16 10:09:12 -0400

    initnal commit

---
+++ variables.py
@@ -0,0 +1,7 @@
+num_apples = 20
+num_orange = 15
+print("Number of apples:", num_apples)
+print("Number of oranges:", num_orange)
+num_orange = 0
+print("Number of apples:", num_apples)
+print("Number of oranges:", num_orange)
